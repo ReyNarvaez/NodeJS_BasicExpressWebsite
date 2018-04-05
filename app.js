cf0e27f16132ce11cd872b8c2e956f4310828ab7
reynarvaez <reynaldo_narvaez@hotmail.com>
2018-04-05 18:43:32 -0400

    added about page

--- app.js
+++ app.js
@@ -16,5 +16,9 @@ app.get('/',function(req,res){
 	res.render('index');
 });
 
+app.get('/about',function(req,res){
+	res.render('about');
+});
+
 app.listen(3000);
 console.log('Server is running on port 3000');
