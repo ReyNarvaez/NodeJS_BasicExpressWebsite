cef5bc5f49c3d278be71ed7e0ab6992d3b94f0e3
reynarvaez <reynaldo_narvaez@hotmail.com>
2018-04-05 17:38:16 -0400

    added express and pug as views engine

--- README.md
+++ README.md
@@ -4,3 +4,5 @@ Basic Express Website Implemented in NodeJS
 ## Technologies Used:
 
 * node v8.2.1
+* express 4.15.5
+* pug 2.0.3
---
+++ app.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var path = require('path');
+var bodyParser = require('body-parser');
+var nodeMailer = require('nodemailer');
+
+var app = express();
+
+app.set('views', path.join(__dirname,'views'));
+app.set('view engine', 'pug');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({'extended':false}));
+
+app.get('/',function(req,res){
+	res.render('index');
+});
+
+app.listen(3000);
+console.log('Server is running on port 3000');
